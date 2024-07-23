f5a0784844bb1f983e395d5785aab86c40929a1a
papunbiswal <biswalgyanaranjan2005@gmail.com>
2024-07-23 15:44:20 +0530

    add: Discord_project1

---
+++ script.js
@@ -0,0 +1,62 @@
+/*body{
+    font-family: "Robot",sans-serif;
+}*/
+/*Navigation*/
+.list-non-bullet{
+    list-style: none;
+}
+.list-item-inline{
+    display: inline;
+    padding: 0rem 0.5rem;
+}
+.navigation .nav-pills{
+    text-align: right;
+    margin:0.6rem 1rem;
+}
+.navigation .link{
+    color:black;
+}
+.navigation .link1{
+    color:red;
+}
+.navigation .link-activi{
+    font-weight: bold;
+}
+.nav-brand a{
+    text-decoration: none;
+    color: inherit;
+}
+.list-item-inline a{
+    text-decoration: none;
+}
+
+.navigation .nav-brand{
+    font-size: 30px;
+    display: flex;
+}
+
+/*CONTENT*/
+#data-details {
+    font-size: 16px;
+    margin-left: 300px;
+    margin-top: 200px;
+    font-family: Segoe UI Semibold;
+    font-weight: 800;
+}
+.link-1{
+    background-color: black;
+    max-width: 140px;
+    padding: 0.6rem 1.2rem;
+    color: rgb(218, 63, 29);
+    margin: 1rem auto;
+    border-radius: 2rem;
+    margin-left: 200px;
+}
+.link-2{
+    background-color:white ;
+    max-width: 140px;
+    padding: 0.6rem 1.2rem;
+    color:rgb(218, 63, 29);
+    margin: 1rem auto;
+    border-radius: 2rem;
+}
